5f8319c2cc9ab771ab143a88ff766a07ed36efed
hutengqi <hutengqi@libawall.cn>
2021-10-15 17:57:10 +0800

    logback配置注释添加及每日LeetCode

--- Boot/src/main/java/cn/sincerity/boot/BootApplication.java
+++ Boot/src/main/java/cn/sincerity/boot/BootApplication.java
@@ -5,17 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @Slf4j
 public class BootApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BootApplication.class, args);
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        StringBuilder stringBuilder = new StringBuilder();
+        Arrays.asList(beanDefinitionNames).forEach(x -> stringBuilder.append(x).append("\n"));
         int length = context.getBeanDefinitionNames().length;
         log.trace("Spring boot启动初始化了 {} 个 Bean", length);
         log.debug("Spring boot启动初始化了 {} 个 Bean", length);
         log.info("Spring boot启动初始化了 {} 个 Bean", length);
         log.warn("Spring boot启动初始化了 {} 个 Bean", length);
+        log.info("Spring boot 启动的都有{}", stringBuilder);
         log.error("Spring boot启动初始化了 {} 个 Bean", length);
     }
 
