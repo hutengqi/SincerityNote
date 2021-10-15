0630d65427d3e25c3ca00bbe5eb9bebd49d9cff7
hutengqi <hutengqi@libawall.cn>
2021-10-15 10:19:16 +0800

    修改忽略文件，添加日志配置及profile 配置。

---
+++ Boot/src/main/java/cn/sincerity/boot/BootApplication.java
@@ -0,0 +1,22 @@
+package cn.sincerity.boot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@Slf4j
+public class BootApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(BootApplication.class, args);
+        int length = context.getBeanDefinitionNames().length;
+        log.trace("Spring boot启动初始化了 {} 个 Bean", length);
+        log.debug("Spring boot启动初始化了 {} 个 Bean", length);
+        log.info("Spring boot启动初始化了 {} 个 Bean", length);
+        log.warn("Spring boot启动初始化了 {} 个 Bean", length);
+        log.error("Spring boot启动初始化了 {} 个 Bean", length);
+    }
+
+}
---
+++ Boot/src/test/java/cn/sincerity/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sincerity.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
